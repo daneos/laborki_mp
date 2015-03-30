bd6e4c46b55ec016b5c38dbd6d057b43014fb676
daneos <daneos@daneos.com>
2015-03-30 16:05:03 +0200

    algorytm wg. cormena

---
+++ lab4/02/main.cpp
@@ -0,0 +1,100 @@
+/*
+ * Metody Programowania - Laboratorium 4 - Program 02
+ * Grzegorz Kowalski, 12i
+ * wersja 1 | 03.2015
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "quicksort.h"
+
+typedef struct _statystyka {
+	int n;			// ilosc elementow
+	int por;		// ilosc porownan
+	double czas;	// czas obliczen
+} statystyka;
+
+statystyka quicksort(long *T, int n)
+{
+	statystyka S;
+	S.n = n;
+	time_t start = time(NULL);
+
+	S.por = _quicksort(T, 0, n-1);		// rzeczywiste sortowanie
+
+	time_t stop = time(NULL);
+	S.czas = difftime(stop, start);	// przenosnosc, przenosnosc i jeszcze raz przenosnosc
+	return S;
+}
+
+void wypisz(long *T, int n, FILE *fout)
+{
+	for(int i=0; i < n; i++)
+		fprintf(fout, "%d ", T[i]);
+	fprintf(fout, "\n");
+}
+
+void wypisz_stat(statystyka *S)
+{
+	printf("%d,%d,%.f\n", S->n, S->por, S->czas);
+}
+
+void blad_danych(void)
+{
+	printf("Blad danych wejsciowych.\n");
+	exit(1);
+}
+
+void sortuj(FILE *fin, FILE *fout)
+{
+	int z;
+	fscanf(fin, "%d", &z);			// ilosc zestawow
+	if(z < 1) blad_danych();
+
+	for(int iz=0; iz < z; iz++)
+	{
+		int n;
+		fscanf(fin, "%d", &n);		// ilosc elementow
+		if(n < 1 || n > 5000000) blad_danych();
+
+		long *T = (long*)malloc(n*sizeof(long));	// rezerwacja pamieci
+		for(int ie=0; ie < n; ie++)
+			fscanf(fin, "%ld", &T[ie]);			// element
+
+		statystyka st = quicksort(T, n);		// sortowanie
+		wypisz(T, n, fout);		// wyjscie
+		wypisz_stat(&st);		// wyjscie danych statystycznych
+
+		free(T);	// zwolnienie pamieci
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc < 2)	// nie podano nazwy pliku wejsciowego
+	{
+		printf("Uzycie: %s <nazwa_pliku_wej> [nazwa_pliku_wyj]\n", argv[0]);
+		exit(1);
+	}
+
+	FILE *in = NULL, *out = NULL;
+	if((in = fopen(argv[1], "r")) == NULL)	// otwarcie tylko do odczytu
+	{
+		perror("main()/fopen():in");
+		exit(1);
+	}
+
+	if(argc < 3) out = stdout;	// jesli nie podano nazwy pliku wyjsciowego, wypisz na ekran
+	else if((out = fopen(argv[2], "w")) == NULL)	// otwarcie tylko do zapisu
+	{
+		perror("main()/fopen():out");
+		exit(1);
+	}
+
+	sortuj(in, out);	// odczyt pliku, obliczenia i wyjscie
+
+	fclose(in);		// zamkniecie plikow
+	fclose(out);
+	return 0;
+}
---
+++ lab4/02/quicksort.h
@@ -0,0 +1,56 @@
+/*
+ * Metody Programowania - Laboratorium 4 - Program 02
+ * Implementacja algorytmu Quicksort
+ * Grzegorz Kowalski, 12i
+ * wersja 1 | 03.2015
+ */
+
+#ifndef __QUICKSORT_H__
+#define __QUICKSORT_H__
+
+int podzial(long *T, int p, int r)
+{
+	return (p+r)/2;
+}
+
+void zamien(long *T, int i, int j)
+{
+	long tmp = T[i];
+	T[i] = T[j];
+	T[j] = tmp;
+}
+
+int dziel(long *T, int p, int r, int *q)
+{
+	int por = 0;
+	int ep = T[p];					// wartosc elementu w punkcie podzialu
+
+	int i = p-1;
+	int j = r+1;
+	for(;;)
+	{
+		while(T[--j] > ep) por++;
+		while(T[++i] < ep) por++;
+		if(i < j) zamien(T, i, j);	// przenoszenie elementow do odpowiednich czesci tablicy
+		else
+		{
+			*q = j;			// ostateczny punkt podzialu
+			return por;
+		}
+	}
+}
+
+int _quicksort(long *T, int p, int r)
+{
+	int por = 0;	// ilosc porownan
+	if(p < r)
+	{
+		int q;
+		por += dziel(T, p, r, &q);		// podziel tablice
+		por += _quicksort(T, p, q-1);		
+		por += _quicksort(T, q+1, r);	// wywolania rekurencyjne
+	}
+	return por;
+}
+
+#endif /* __QUICKSORT_H__ */
