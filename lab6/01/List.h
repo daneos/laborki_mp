a2a85e6f38fd6b05b567162714a549ac510d5882
daneos <daneos@daneos.com>
2015-04-17 15:18:50 +0200

    lista

---
+++ lab6/01/List.h
@@ -0,0 +1,168 @@
+/*
+ * Metody Programowania - Laboratorium 6 - Program 01
+ * Implementacja klas: lista
+ * Grzegorz Kowalski, 12i
+ * wersja 1 | 04.2015
+ */
+
+#include <stdio.h>
+
+#ifndef __LIST_H__
+#define __LIST_H__
+
+// Definicje
+namespace List
+{
+	template <class T>
+	class List;
+
+	template <class T>
+	class Element
+	{
+		friend class List<T>;
+	private:
+		T data;
+		Element<T> *p_next;
+
+	public:
+		Element(T data);
+		~Element();
+		Element<T> *next(void);
+		T *getData(void);
+		void remove(void);
+	};
+
+	template <class T>
+	class List
+	{
+	private:
+		Element<T> *root;
+		int len;
+
+	public:
+		List();
+		~List();
+		Element<T> *get(int i);
+		int getLen(void);
+		int append(Element<T> *e);
+		int prepend(Element<T> *e);
+		int appendAfter(Element<T> *e, int i);
+		int prependBefore(Element<T> *e, int i);
+		int remove(int i);
+		int operator+=(Element<T> *e);
+	};
+
+	// Implementacja
+
+	// ----- class Element
+	template <class T>
+	Element<T>::Element(T data)
+	{
+		this->data = data;
+		this->p_next = NULL;
+	}
+
+	template <class T>
+	Element<T>::~Element()
+	{
+	}
+
+	template <class T>
+	Element<T> *Element<T>::next(void)
+	{
+		return this->p_next;
+	}
+
+	template <class T>
+	T *Element<T>::getData(void)
+	{
+		return &this->data;	// zwracam wskaznik do danych, bo dane moga byc typu zlozonego
+	}
+
+	// ----- class List
+	template <class T>
+	List<T>::List()
+	{
+		this->root = NULL;
+		this->len = 0;
+	}
+
+	template <class T>
+	List<T>::~List()
+	{
+	}
+
+	template <class T>
+	Element<T> *List<T>::get(int i)
+	{
+		if(i <= this->len && i > 0)
+		{
+			Element<T> *e = this->root;
+			for(int j=1; j<i; j++) e = e->next();	// przejdz do i-tego elementu listy
+			return e; 
+		}
+		return NULL;	// jesli i jest wieksze niz dlugosc listy (lub ujemne) zwroc pusty wskaznik
+	}
+
+	template <class T>
+	int List<T>::getLen(void)
+	{
+		return this->len;
+	}
+
+	template <class T>
+	int List<T>::append(Element<T> *e)
+	{
+		if(this->len == 0)
+			this->root = e;		// jesli lista jest pusta nowy element staje sie glowa
+		else
+			this->get(this->len)->p_next = e;	// jesli nie, dolaczany jest do ostatniego elementu
+		return ++this->len;
+	}
+
+	template <class T>
+	int List<T>::prepend(Element<T> *e)
+	{
+		e->p_next = this->root;		// lista dolaczana jest do nowego elementu
+		this->root = e;				// nowy elelment staje sie glowa
+		return ++this->len;
+	}
+
+	template <class T>
+	int List<T>::appendAfter(Element<T> *e, int i)
+	{
+		Element<T> *after;
+		if((after = this->get(i)) == NULL) return -1;	// jesli element za ktory wstawiam nie istnieje, zwroc blad
+		e->p_next = after->next();		// nowy element wskazuje na nastepnik poprzedniego
+		after->p_next = e;			// poprzedni wskazuje na nowy
+		return ++this->len;
+	}
+
+	template <class T>
+	int List<T>::prependBefore(Element<T> *e, int i)
+	{
+		return this->appendAfter(e, i-1);	// dodanie przed i-ty element mozna sprowadzic do dodania za (i-1)
+	}
+
+	template <class T>
+	int List<T>::remove(int i)
+	{
+		if(i <= this->len && i > 0)
+		{
+			Element<T> *prev = this->get(i-1);
+			Element<T> *current = prev->next();
+			prev->p_next = current->next();		// element poprzedni dostaje adres nastepnego
+			delete current;		// aktualny jest usuwany
+			return --this->len;
+		}
+		return -1;	// element nie istnieje
+	}
+
+	template <class T>
+	int List<T>::operator+=(Element<T> *e)
+	{
+		return this->append(e);
+	}
+}
+
+#endif /* __LIST_H__ */
