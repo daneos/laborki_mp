61dc7f269d31789bbb086904113f5f21b7be032b
daneos <daneos@daneos.com>
2015-04-24 20:51:58 +0200

    chyba nie dziala

--- lab7/01/main.cpp
+++ lab7/01/main.cpp
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "List.h"
 
 typedef struct _przedmiot {
@@ -30,9 +31,63 @@ int czytaj(FILE *fin, List::List<przedmiot> *L)
 	return poj;
 }
 
-void zapakuj(int p, List::List<przedmiot> *L)
+void wypisz(FILE *fout, int **tab, int x, int y)
 {
+	for(int i=0; i <= x; i++)
+	{
+		for(int j=0; j <= y; j++)
+			fprintf(fout, "%d\t", tab[i][j]);
+		fprintf(fout, "\n");
+	}
+}
 
+void zapakuj(FILE *fout, int p, List::List<przedmiot> *L)
+{
+	int **P = (int**)malloc((L->getLen()+1)*sizeof(int*));		// alokacja i inicjalizacja P
+	for(int i=0; i <= L->getLen(); i++)
+	{
+		P[i] = (int*)malloc((p+1)*sizeof(int));
+		memset(P[i], 0, (p+1)*sizeof(int));
+	}
+
+	int **Q = (int**)malloc((L->getLen()+1)*sizeof(int*));		// alokacja i inicjalizacja Q
+	for(int i=0; i <= L->getLen(); i++)
+	{
+		Q[i] = (int*)malloc((p+1)*sizeof(int));
+		memset(Q[i], 0, (p+1)*sizeof(int));
+	}
+
+	L->reset();
+	for(int i=1; L->current(); i++)
+	{
+		for(int j=1; j <= p; j++)
+		{
+			przedmiot *a = L->current()->getData(); 
+			if((j >= a->masa) && (P[i-1][j] < P[i][j-a->masa] + a->cena))
+			{
+				P[i-1][j] = P[i][j-a->masa] + a->cena;	// dodanie aktualnego przedmiotu
+				Q[i][j] = i;
+			}
+			else
+			{
+				P[i][j] = P[i-1][j];	// przepisanie wartosci poprzednich
+				Q[i][j] = Q[i-1][j];
+			}
+		}
+		L->next();
+	}
+
+	wypisz(fout, P, L->getLen(), p);
+	fprintf(fout, "\n\n");
+	wypisz(fout, Q, L->getLen(), p);
+
+	for(int i=0; i < L->getLen(); i++)
+		free(P[i]);
+	free(P);		// zwolnienie P
+
+	for(int i=0; i < L->getLen(); i++)
+		free(Q[i]);
+	free(Q);		// zwolnienie Q
 }
 
 int main(int argc, char *argv[])
@@ -59,7 +114,7 @@ int main(int argc, char *argv[])
 
 	List::List<przedmiot> *L = new List::List<przedmiot>;
 	int pojemnosc = czytaj(in, L);		// wczytywanie rzeczy
-	zapakuj(pojemnosc, L);		// pakowanie plecaka
+	zapakuj(out, pojemnosc, L);		// pakowanie plecaka
 
 	delete L;
 	fclose(in);		// zamkniecie plikow
