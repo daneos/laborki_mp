16de691b9d4852acf16be24128e9e5d9a5d1bd6b
daneos <daneos@daneos.com>
2015-05-11 23:41:48 +0200

    funkcje: k, totweight, bound

--- lab8/01/main.cpp
+++ lab8/01/main.cpp
@@ -23,13 +23,13 @@ typedef struct _wezel {
 	int k;
 } wezel;
 
-
-void wypisz_liste(List::List<przedmiot> *L)
+void wypisz_liste(FILE *fout, List::List<przedmiot> *L)
 {
+	fprintf(fout, "    id\tcena\tmasa\t\tadres\n    --------------------------------------------------\n");
 	for(L->reset(); L->current(); L->next())
 	{
 		przedmiot *p = L->current()->getData();
-		printf("%p, %d, %d, %d\n", p, p->i, p->cena, p->masa);
+		fprintf(fout, "    %d\t%d\t%d\t\t%p\n", p->i, p->cena, p->masa, p);
 	}
 }
 
@@ -63,9 +63,57 @@ int czytaj(FILE *fin, List::List<przedmiot> *L)
 	return poj;
 }
 
+int k(Tree::Node::BinaryReturn<wezel> *W, int p, List::List<przedmiot> *L)
+{
+	int total = 0;
+	int k;
+	for(k=1; k <= L->getLen(); k++)
+	{
+		if(W->key->Depth < k || BITMAP_CHECK(W->key->Bitmap, k))
+			total += (*L)[k]->getData()->masa;
+		if(total >= p) break;
+	}
+	return total < p ? k+1 : k;
+}
+
+int totweight(Tree::Node::BinaryReturn<wezel> *W, int p, List::List<przedmiot> *L)
+{
+	int t = W->getData()->weight;
+	for(int i = W->key->Depth+1; i <= k(W, p, L)-1; i++)
+		t += (*L)[i]->getData()->masa;
+	return t;
+}
+
+int bound(Tree::Node::BinaryReturn<wezel> *W, int p, List::List<przedmiot> *L)
+{
+	int b = W->getData()->profit;
+	int k_w = k(W, p, L);
+	for(int i = W->key->Depth+1; i <= k_w-1; i++)
+		b += (*L)[i]->getData()->cena;
+	return b + (p - totweight(W, p, L)) * ((*L)[k_w]->getData()->cena/(*L)[k_w]->getData()->masa);
+}
+
+void wypisz_wezel_info(FILE *fout, Tree::Node::BinaryReturn<wezel> *W, int p, List::List<przedmiot> *L)
+{
+	wezel *w_data = W->getData();
+	fprintf(fout, "[:] Wezel: (%d, %d)\t\tNode:%p\t\tData:%p\t\tP:%p\n", W->key->Bitmap, W->key->Depth, W, w_data, w_data->P);
+	fprintf(fout, "    k         = %d\n", w_data->k);
+	fprintf(fout, "    totweight = %d\n", totweight(W, p, L));
+	fprintf(fout, "    bound     = %d\n", w_data->bound);
+}
+
 void zapakuj(FILE *fout, int W, List::List<przedmiot> *L)
 {
+	fprintf(fout, "[:] Inicjalizacja drzewa...\n");
+	wezel root = { NULL, 0, 0, 0, 0 };		// inicjalizacja drzewa
+	Tree::BinaryReturn<wezel> *T = new Tree::BinaryReturn<wezel>;
+	Tree::Node::BinaryReturn<wezel> *r = new Tree::Node::BinaryReturn<wezel>(root);
+	T->setRoot(r);
+	T->Reset();
 
+	T->Current()->getData()->k = k(T->Current(), W, L);
+	T->Current()->getData()->bound = bound(T->Current(), W, L);
+	wypisz_wezel_info(fout, r, W, L);
 }
 
 int main(int argc, char *argv[])
@@ -92,9 +140,10 @@ int main(int argc, char *argv[])
 
 	List::List<przedmiot> *L = new List::List<przedmiot>;
 	int pojemnosc = czytaj(in, L);		// wczytywanie rzeczy
+	fprintf(out, "[+] Wczytano plecak: %d\n", pojemnosc);
+	wypisz_liste(out, L);
+	printf("\n\n");
 	zapakuj(out, pojemnosc, L);		// pakowanie plecaka
-
-	wypisz_liste(L);
 
 	delete L;
 	fclose(in);		// zamkniecie plikow
